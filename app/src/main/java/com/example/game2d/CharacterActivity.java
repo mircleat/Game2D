e4eaa2f29312606181a9f907c77c75ee8bc79428
pip <ppp@bu.edu>
2023-04-24 23:08:34 -0400

    major UI update (splash,title,character selection)

---
+++ app/src/main/java/com/example/game2d/CharacterActivity.java
@@ -0,0 +1,52 @@
+package com.example.game2d;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CharacterActivity extends AppCompatActivity { //
+
+
+    Button boy_btn, girl_btn, confirm_btn;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_selection); //
+
+        // Continue button
+        Button back_button = (Button) findViewById(R.id.confirm_btn);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // switch back to main activity
+                Intent intent = new Intent(CharacterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+    }
+
+    //This part does not work it is supposed to go back to the main Activity
+    public void onClick(View view) {
+        Button clickedButton = (Button) view;
+        if (clickedButton.getId() == R.id.confirm_btn) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+    }
+
+
+}
