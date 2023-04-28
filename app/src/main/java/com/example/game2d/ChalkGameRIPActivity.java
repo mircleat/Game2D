db3cddbdf7b000da365945b54bc6bb692e330489
kita.t1509 <ankita04@bu.edu>
2023-04-27 21:21:29 -0400

    Added ChalkRIP page

---
+++ app/src/main/java/com/example/game2d/ChalkGameRIPActivity.java
@@ -0,0 +1,26 @@
+package com.example.game2d;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChalkGameRIPActivity extends AppCompatActivity {
+    private Button resButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chalk_game_ripactivity);
+        resButton = findViewById(R.id.ChalkRIPResume);
+        resButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ChalkRIPtrns = new Intent(ChalkGameRIPActivity.this,ChalkGameActivity.class);
+                ChalkRIPtrns.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(ChalkRIPtrns);
+            }
+        });
+    }
+}
