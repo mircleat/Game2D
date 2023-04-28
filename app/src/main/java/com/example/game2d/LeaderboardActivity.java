e90943bf657f5c6f5279549050bb755e4087473f
pip <ppp@bu.edu>
2023-04-28 00:52:31 -0400

    leaderboard UI

--- app/src/main/java/com/example/game2d/LeaderboardActivity.java
+++ app/src/main/java/com/example/game2d/LeaderboardActivity.java
@@ -10,6 +10,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -24,7 +25,7 @@ import java.util.Vector;
 public class LeaderboardActivity extends AppCompatActivity {
 
     //big_userScoreData
-    private LinearLayout scoreListLayout;
+    private LinearLayout scoreListLayout,scoreListLayout2,scoreListLayout3;
 
     @SuppressLint("DefaultLocale")
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -37,6 +38,8 @@ public class LeaderboardActivity extends AppCompatActivity {
 
         // Get reference to the LinearLayout that will hold the scores
         scoreListLayout = findViewById(R.id.score_list_layout);
+        scoreListLayout2 = findViewById(R.id.score_list_layout2);
+        scoreListLayout3 = findViewById(R.id.score_list_layout3);
 
         // Create map to store usernames and scores
 
@@ -67,16 +70,35 @@ public class LeaderboardActivity extends AppCompatActivity {
         HashMap<String, Integer> sortedScoreMap = new HashMap<String, Integer>();
         sortedScoreMap = (HashMap<String, Integer>) sortByValue(scoreMap);
 
+        int ranking = 0;
         for (String username : scoreboardObj.scoreboard.keySet()) {
+            ranking++;
             Log.d("SCORETEXT", username + String.valueOf(scoreboardObj.scoreboard.get("scrub daddy")));
             // Create a textview for the username and score
+            TextView rankView = new TextView(this);
+            rankView.setTextSize(20);
+            rankView.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            rankView.setFontFeatureSettings("press_start_2p");
+            rankView.setText(String.format("%d",ranking));
+
+            TextView nameView = new TextView(this);
+            nameView.setTextSize(20);
+            nameView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            nameView.setFontFeatureSettings("press_start_2p");
+            nameView.setText(String.format("%s", username));
+
             TextView scoreView = new TextView(this);
             scoreView.setTextSize(20);
-            scoreView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            scoreView.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
             scoreView.setFontFeatureSettings("press_start_2p");
-            scoreView.setText(String.format("%s: %f", username, scoreboardObj.scoreboard.get(username)));
+            scoreView.setText(String.format("%f",  scoreboardObj.scoreboard.get(username)));
+
+
+
             // Add the TextView to the LinearLayout
-            scoreListLayout.addView(scoreView);
+            scoreListLayout.addView(rankView);
+            scoreListLayout2.addView(nameView);
+            scoreListLayout3.addView(scoreView);
         }
 
         /*
@@ -93,5 +115,17 @@ public class LeaderboardActivity extends AppCompatActivity {
         }
 
          */
+
+
+        // Return button
+        Button confirm_button = (Button) findViewById(R.id.return_btn);
+        confirm_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // switch back to main activity
+                Intent intent = new Intent(LeaderboardActivity.this, FirebaseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
