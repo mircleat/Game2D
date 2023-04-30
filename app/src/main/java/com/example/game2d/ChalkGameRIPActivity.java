d367de15e0958547c015db5ad20a49bee2da0ce4
Pranav Shrihari <pranavsh@bu.edu>
2023-04-29 20:02:17 -0400

    Fullscreen part 2

--- app/src/main/java/com/example/game2d/ChalkGameRIPActivity.java
+++ app/src/main/java/com/example/game2d/ChalkGameRIPActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class ChalkGameRIPActivity extends AppCompatActivity {
@@ -20,6 +22,12 @@ public class ChalkGameRIPActivity extends AppCompatActivity {
         ripSound.start();
         setContentView(R.layout.activity_chalk_game_ripactivity);
         resButton = findViewById(R.id.ChalkRIPResume);
+
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
         resButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
