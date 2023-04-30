0c9b84bfe8c95431f5c5a393d40eff2f6c45349a
ethan <ethanl66@bu.edu>
2023-04-30 17:04:18 -0400

    mock

--- app/src/test/java/com/example/game2d/ExampleUnitTest.java
+++ app/src/test/java/com/example/game2d/ExampleUnitTest.java
@@ -1,17 +1,47 @@
 package com.example.game2d;
 
+import org.junit.Rule;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
+//
+import android.content.Context;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+//@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
-    @Test
+    @Mock
+    ChalkActivity testObj;
+
+     /*@Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }*/
+
+    @Test
+    public void randomizeOrderTest() {
+        int[] original = new int[] { 0, 1, 2, 3 };
+
+        testObj = new ChalkActivity();
+        assertNotEquals(original, testObj.randomizeOrders());
     }
 }
