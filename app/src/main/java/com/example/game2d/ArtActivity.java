239292e11c817d7fd8b782ff86637852701035d3
eb <eberrios@bu.edu>
2023-05-01 17:46:09 -0400

    Documentation for ArtActivity

--- app/src/main/java/com/example/game2d/ArtActivity.java
+++ app/src/main/java/com/example/game2d/ArtActivity.java
@@ -10,27 +10,30 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ArtActivity extends AppCompatActivity { //
+/**
+ * ArtActivity is a credit page for the game's art.
+ * It is triggered when the player approaches the painting.
+ */
+public class ArtActivity extends AppCompatActivity {
 
     @SuppressLint("SetTextI18n")
     @Override
-    //for back button
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_art);
-
+        // Hide status bar
         Window window = getWindow();
         window.setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
 
-        // back button
+        // Back button
         Button back_button = (Button) findViewById(R.id.return_btn);
         back_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // switch back to main activity
+                // Switch back to main activity
                 Intent intent = new Intent(ArtActivity.this, MainActivity.class);
                 startActivity(intent);
             }
